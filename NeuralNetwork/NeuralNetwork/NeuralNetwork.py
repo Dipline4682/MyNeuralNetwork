62373600ef2be63f56a715366da6ccc1ff72cd27
Dipline4682 <36309408+Dipline4682@users.noreply.github.com>
2019-11-02 00:43:56 +0500

    Update NeuralNetwork.py

--- NeuralNetwork/NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork/NeuralNetwork.py
@@ -4,16 +4,16 @@ import numpy as np
 
 def CreatWeights(l):#h, w):
     mas = []
-    for j in range(l*100):
-        #mas.append(random.randint(a, b)) #Рандом Bias
-        mas.append(float("{0:.1f}".format(random.uniform(-5.0, 5.0))))#[random.uniform(0.0, 2.0) for j in range(w)])
+    for j in range(l*10):
+        mas.append(np.random.normal()) #Рандом Bias
+        #mas.append(float("{0:.1f}".format(np.random.normal())))#[random.uniform(0.0, 2.0) for j in range(w)])
         #mas.append([float("{0:.1f}".format(random.uniform(-2.0, 2.0))) for i in range(l)])#[random.uniform(0.0, 2.0) for j in range(w)])
     #print(len(mas))
     #mas = np.array(mas)
     return mas
 
 def SetWeightFile(SaveWeight):
-    with open('leyar_weights.txt', 'w') as filehandle:  
+    with open('output_weights.txt', 'w') as filehandle:  
         for listitem in SaveWeight:
             filehandle.write('%s\n' % listitem)
 
@@ -79,23 +79,20 @@ class Neuron:
         for i in range(l):
             input_layer.append(np.dot(self.weights[i], inputs))
         input_layer = np.array(input_layer)
-        print()
-        print(input_layer)
-        print()
         return sigmoid(input_layer)
  
-#class OurNeuralNetwork:
- #   def __init__(self, weights, bias):
-  #      self.weights = weights
-   #     self.bias = bias
-    #    #self.h1 = Neuron(weights, bias)
-     #   #self.h2 = Neuron(weights, bias)
-      #  self.o1 = Neuron(np.array([random.uniform(0, 10), random.uniform(0, 10)]), bias)
-    #def feedforward(self, x):
-     #   #out_h1 = self.h1.feedforward(x)
-      #  #out_h2 = self.h2.feedforward(x)
-       # #out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
-        #return out_o1
+class OurNeuralNetwork:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.o1 = Neuron(np.array([random.uniform(0, 10), random.uniform(0, 10)]), bias)
+    def feedforward(self, x):
+        out_h1 = self.h1.feedforward(x)
+        out_h2 = self.h2.feedforward(x)
+        out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
+        return out_o1
 
 
 img = cv2.imread('2.png', 0)
@@ -122,13 +119,14 @@ out_layer = Neuron(GetWeightFile('output_weights'), GetWeightFile('bias_weights'
 print(out_layer.feedforward(slay_l, len(GetWeightFile('output_weights'))))
 
 
+
+# Тренируем нашу нейронную сеть!
+#network = OurNeuralNetwork()
+#network.train(data, all_y_trues)
+
+
+
 #SetWeightFile(CreatWeights(h*w)) #Генерация и запись в файл случайных значений для весов
 #print(GetWeightFile('leyar_weights')) # Считывание с файла массива весов //bias_weights //input_weights //output_weights
-#print(CreatWeights(h*w))
-#network = OurNeuralNetwork(weights, bias)
-#print(network.feedforward(mas))
-#random.uniform(0, 10)
-#w = w.reshape((10, 10))
-#print(w)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
