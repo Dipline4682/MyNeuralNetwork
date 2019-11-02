c99159c91cfbda17df191062ca0430fc0c3cae05
Dipline4682 <36309408+Dipline4682@users.noreply.github.com>
2019-11-03 00:24:47 +0500

    Add files via upload

--- NeuralNetwork/NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork/NeuralNetwork.py
@@ -4,38 +4,77 @@ import numpy as np
 
 def CreatWeights(l):#h, w):
     mas = []
-    for j in range(l*10):
+    for j in range(l):
         mas.append(np.random.normal()) #Рандом Bias
-        #mas.append(float("{0:.1f}".format(np.random.normal())))#[random.uniform(0.0, 2.0) for j in range(w)])
+        #mas.append(float("{0:.1f}".format(random.uniform(-1.0, 1.0))))#[random.uniform(0.0, 2.0) for j in range(w)])
         #mas.append([float("{0:.1f}".format(random.uniform(-2.0, 2.0))) for i in range(l)])#[random.uniform(0.0, 2.0) for j in range(w)])
     #print(len(mas))
     #mas = np.array(mas)
     return mas
 
 def SetWeightFile(SaveWeight):
-    with open('output_weights.txt', 'w') as filehandle:  
+    with open('output_bias_weight.txt', 'w') as filehandle:  
         for listitem in SaveWeight:
             filehandle.write('%s\n' % listitem)
 
 def GetWeightFile (puty):
+    # Загрузка выходных весов
     if puty == 'output_weights':
     # определим пустой список
-        weights = []
-        weights.clear()
-        input_weights = []
+        output_weights = []
+        output_weights.clear()
         # откроем файл и считаем его содержимое в список
         with open('output_weights.txt', 'r') as filehandle:  
             for line in filehandle:
                 # удалим заключительный символ перехода строки
                 currentPlace = line[:-1]
                 # добавим элемент в конец списка
-                weights.append(float(currentPlace))
+                output_weights.append(float(currentPlace))
         #weights = np.array(weights)
-        weights = np.reshape(weights, (10, 100))
-        return weights
+        output_weights = np.reshape(output_weights, (10, 100))
+        return output_weights
+    # Загрузка входных весов смещения
+    elif puty == 'input_bias_weight':
+    # определим пустой список
+        input_bias_weight = []
+        input_bias_weight.clear()
+        # откроем файл и считаем его содержимое в список
+        with open('input_bias_weight.txt', 'r') as filehandle:  
+            for line in filehandle:
+                # удалим заключительный символ перехода строки
+                currentPlace = line[:-1]
+                # добавим элемент в конец списка
+                input_bias_weight.append(float(currentPlace))
+        return input_bias_weight
+    # Загрузка весов смещения скрытого слоя
+    elif puty == 'leyar_bias_weight':
+    # определим пустой список
+        leyar_bias_weight = []
+        leyar_bias_weight.clear()
+        # откроем файл и считаем его содержимое в список
+        with open('leyar_bias_weight.txt', 'r') as filehandle:  
+            for line in filehandle:
+                # удалим заключительный символ перехода строки
+                currentPlace = line[:-1]
+                # добавим элемент в конец списка
+                leyar_bias_weight.append(float(currentPlace))
+        return leyar_bias_weight
+    # Загрузка весов выходного слоя
+    elif puty == 'output_bias_weight':
+    # определим пустой список
+        output_bias_weight = []
+        output_bias_weight.clear()
+        # откроем файл и считаем его содержимое в список
+        with open('output_bias_weight.txt', 'r') as filehandle:  
+            for line in filehandle:
+                # удалим заключительный символ перехода строки
+                currentPlace = line[:-1]
+                # добавим элемент в конец списка
+                output_bias_weight.append(float(currentPlace))
+        return output_bias_weight
+    # Загрузка входных весов
     elif puty == 'input_weights':
     # определим пустой список
-        weights = []
         input_weights = []
         input_weights.clear()
         # откроем файл и считаем его содержимое в список
@@ -44,12 +83,12 @@ def GetWeightFile (puty):
                 # удалим заключительный символ перехода строки
                 currentPlace = line[:-1]
                 # добавим элемент в конец списка
-                weights.append(float(currentPlace))
-        input_weights = np.reshape(weights, (100, 100))
+                input_weights.append(float(currentPlace))
+        input_weights = np.reshape(input_weights, (100, 100))
         return input_weights
+    # Загрузка весов скрытого слоя
     elif puty == 'leyar_weights':
     # определим пустой список
-        weights = []
         leyar_weights = []
         leyar_weights.clear()
         # откроем файл и считаем его содержимое в список
@@ -58,8 +97,8 @@ def GetWeightFile (puty):
                 # удалим заключительный символ перехода строки
                 currentPlace = line[:-1]
                 # добавим элемент в конец списка
-                weights.append(float(currentPlace))
-        leyar_weights = np.reshape(weights, (100, 100))
+                leyar_weights.append(float(currentPlace))
+        leyar_weights = np.reshape(leyar_weights, (100, 100))
         return leyar_weights
 
 def sigmoid(x):
@@ -76,18 +115,23 @@ class Neuron:
     def feedforward(self, inputs, l):
         input_layer = []
         input_layer.clear()
+        weights = []
+        weights.clear()
         for i in range(l):
             input_layer.append(np.dot(self.weights[i], inputs))
         input_layer = np.array(input_layer)
-        return sigmoid(input_layer)
+        for i in range(l):
+            weights.append(input_layer[i] + self.bias[i])
+        weights = np.array(weights)
+        return sigmoid(weights)
  
 class OurNeuralNetwork:
     def __init__(self):
         #self.weights = weights
         #self.bias = bias
-        self.h1 = Neuron(GetWeightFile('input_weights'), GetWeightFile('bias_weights'))
-        self.h2 = Neuron(GetWeightFile('leyar_weights'), GetWeightFile('bias_weights'))
-        self.o1 = Neuron(GetWeightFile('output_weights'), GetWeightFile('bias_weights'))
+        self.h1 = Neuron(GetWeightFile('input_weights'), GetWeightFile('input_bias_weight'))
+        self.h2 = Neuron(GetWeightFile('leyar_weights'), GetWeightFile('leyar_bias_weight'))
+        self.o1 = Neuron(GetWeightFile('output_weights'), GetWeightFile('output_bias_weight'))
     def feedforward(self, x):
         out_h1 = self.h1.feedforward(x, len(GetWeightFile('input_weights')))
         print(out_h1)
@@ -103,12 +147,14 @@ img = cv2.imread('2.png', 0)
 h, w = img.shape
 ret,img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 cv2.imshow('qwe', img)
+#print(img)
 
 img1d = []
 for i  in range(h):
     for j in range(w):
-        img1d.append(img[i][j])
-
+        if img[i][j] == 255: img1d.append(img[i][j] - 254)
+        else: img1d.append(img[i][j])
+#print(img1d)
 # Передача веса и смещения в нейрон
 #imput_layer = Neuron(GetWeightFile('input_weights'), GetWeightFile('bias_weights'))
 #in_l = imput_layer.feedforward(img1d, len(GetWeightFile('input_weights')))
@@ -132,6 +178,8 @@ print(network.feedforward(img1d))
 
 
 #SetWeightFile(CreatWeights(h*w)) #Генерация и запись в файл случайных значений для весов
-#print(GetWeightFile('leyar_weights')) # Считывание с файла массива весов //bias_weights //input_weights //output_weights
+#print(GetWeightFile('leyar_weights')) # Считывание с файла массива весов 
+#//bias_weights //input_weights //output_weights 
+#//input_bias_weight //leyar_bias_weight //output_bias_weight
 cv2.waitKey(0)
 cv2.destroyAllWindows()
