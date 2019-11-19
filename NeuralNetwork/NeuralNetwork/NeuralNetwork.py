a2e23f52ba0ede0526cfefba1764fc53c399b07f
Dipline4682 <36309408+Dipline4682@users.noreply.github.com>
2019-11-20 00:48:33 +0500

    Update NeuralNetwork.py

--- NeuralNetwork/NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork/NeuralNetwork.py
@@ -147,20 +147,40 @@ class OurNeuralNetwork:
         self.h2 = Neuron(GetWeightFile('leyar_weights'), GetWeightFile('leyar_bias_weights'))
         self.o1 = Neuron(GetWeightFile('output_weights'), GetWeightFile('output_bias_weights'))
     def feedforward(self, x):
+        input = np.zeros((10,10), np.uint8)
+        layer = np.zeros((10,10), np.uint8)
+        output = np.zeros((10,10), np.uint8)
+
         out_h1 = self.h1.feedforward(x, len(GetWeightFile('input_weights')))
         print('out_h1')
         print(out_h1)
         print()
+        
+        input = out_h1.copy()
+        input = np.reshape(input, (10, 10))
+        cv2.imwrite('input.png', input)
+
         out_h2 = self.h2.feedforward(out_h1, len(GetWeightFile('leyar_weights')))
         print('out_h2')
         print(out_h2)
         print()
+
+        layer = out_h1
+        layer = np.reshape(layer, (10, 10))
+        cv2.imwrite('layer.png', layer)
+
+
         out_o1 = self.o1.feedforward(out_h2, len(GetWeightFile('output_weights')))
         print('out_o1')
+
+        output = out_h1
+        output = np.reshape(output, (10, 10))
+        cv2.imwrite('output.png', output)
+
         return out_o1
 
 
-img = cv2.imread('3.png', 0)
+img = cv2.imread('2.png', 0)
 img = cv2.resize(img, (10, 10), interpolation = cv2.INTER_AREA)
 
 h, w = img.shape
@@ -171,28 +191,59 @@ cv2.imshow('qwe', img)
 img1d = []
 for i  in range(h):
     for j in range(w):
-        if img[i][j] == 255: img1d.append(img[i][j] - 254)
+        if img[i][j] == 255: img1d.append(img[i][j]-254)
         else: img1d.append(img[i][j])
 
 #Передаём изображение в нейронную сеть
 network = OurNeuralNetwork()
 #print(network.feedforward(img1d))
+otv = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+otv = np.array(otv)
+max = 0
+count = 0
+
 pol = []
 pol = network.feedforward(img1d)
 print(pol)
-otv = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
-otv = np.array(otv)
 print()
-print('MSE')
-print(mse_loss(pol, otv)/1)
-max = 0
-count = 0
+print('MSE = ', mse_loss(pol, otv)/1)
+
 for i in range(len(pol)):
     if pol[i] > max:
         max = pol[i]
         count = i
-print('otvet')
 
+
+if count == 0: 
+    print('otvet 0; Veroyatnosty', pol[count])
+    count = 0
+elif count == 1: 
+    print('otvet 1; Veroyatnosty', pol[count])
+    count = 0
+elif count == 2: 
+    print('otvet 2; Veroyatnosty', pol[count])
+    count = 0
+elif count == 3: 
+    print('otvet 3; Veroyatnosty', pol[count])
+    count = 0
+elif count == 4: 
+    print('otvet 4; Veroyatnosty', pol[count])
+    count = 0
+elif count == 5: 
+    print('otvet 5; Veroyatnosty', pol[count])
+    count = 0
+elif count == 6: 
+    print('otvet 6; Veroyatnosty', pol[count])
+    count = 0
+elif count == 7: 
+    print('otvet 7; Veroyatnosty', pol[count])
+    count = 0
+elif count == 8: 
+    print('otvet 8; Veroyatnosty', pol[count])
+    count = 0
+elif count == 9: 
+    print('otvet 9; Veroyatnosty', pol[count])
+    count = 0
 
 #SetWeightFile(CreatWeights(h*w)) #Генерация и запись в файл случайных значений для весов
 #print(GetWeightFile('leyar_weights')) # Считывание с файла массива весов 
